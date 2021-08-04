bb94e598b7e954962d312caee0f65880198e593f
ppuertot <pedro.puerto@gmail.com>
2021-08-04 13:40:43 -0500

    Delete headers.js

--- src/headers.js
+++
@@ -1,8 +0,0 @@
-let headers = new Headers();
-let url = 'https://sias.alcriservicio.com';
-// let url = 'http://localhost:8000';
-
-headers.append('Authorization', 'Basic ' + Buffer.from('alcri:Au14785236').toString('base64'));
-headers.append('Content-Type', 'application/json');
-
-export {headers, url};
